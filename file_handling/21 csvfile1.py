8a1122cc397826c8677239980fec71449b02031e
Ilamparithi M <5023573ilamparithi@gmail.com>
2023-11-14 20:42:57 +0530

    update 21

--- file_handling/21 csvfile1.py
+++ file_handling/21 csvfile1.py
@@ -1,36 +1,40 @@
 import csv
 
-def enter(n, w):
+def enter(n):
+    f = open("Student.csv", "a", newline='')
+    w = csv.writer(f)
     for c in range(1,n+1):
         print(f"Student {c}")
         Rno = int(input("Roll no.: "))
         Name = input("Name: ")
         print("Marks out of 100:")
-        English = float(input("English marks: "))
-        cs = float(input("CS marks: "))
-        Maths = float(input("Maths marks: "))
-        Physics = float(input("Physics marks: "))
-        Chemistry = float(input("Chemistry marks: "))
-        Average = (English + cs + Maths + Physics + Chemistry)/5
-        w.writerow([Rno, Name, English, cs, Maths, Physics, Chemistry, Average])
+        print("English, CS, Maths, Physics, Chemistry (Comma separated)")
+        E, CS, M, P, C = tuple(eval(input('> ')))
+        Average = (E + CS + M + P + C)/5
+        print("Average:", Average)
+        w.writerow([Rno, Name, E, CS, M, P, C, Average])
         print("Entered.")
     print("Complete.")
+    f.close()
 
-def showtoppers(f):
+def showtoppers():
+    f = open("Student.csv", "r", newline='')
     r = csv.reader(f)
     try:
-        r = next(r)
+        next(r)
         print("\nRno\tName\t\t\tEnglish\tCS\tMaths\tPhysics\tChemistry\tAverage")
         for i in r:
-            if i[-1] > 85:
+            if float(i[-1]) > 85:
                 print(f"\n{i[0]}\t{i[1]}\t\t\t{i[2]}\t{i[3]}\t{i[4]}\t{i[5]}\t{i[6]}\t{i[7]}")
     except StopIteration:
         print("No records")
+    f.close()
 
-f = open("Student.csv", "w+", newline='')
+f = open("Student.csv", "w", newline='')
 w = csv.writer(f)
-header = ["Rno", "Name", "English", "CS", "Maths", "Physics", "Chemistry", "Average"]
+header = ["Rno", "Name", "English", "CS", "Maths", "Physics", "Chem", "Average"]
 w.writerow(header)
+f.close()
 
 while True:
     print("What would you like to do?")
@@ -41,13 +45,14 @@ while True:
     ch = int(input("> "))
     if ch == 1:
         n = int(input("How many records? "))
-        enter(n, w)
+        enter(n)
 
     elif ch == 2:
         print("Students with avg > 85:")
-        showtoppers(f)
+        showtoppers()
         
     elif ch == 3:
+        f.close()
         print("Goodbye!")
         break
 
